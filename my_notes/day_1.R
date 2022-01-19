64692c3d8665d46793fcfb86143ff079cf47cdd7
DylanMG <dylan.glaser1@ucalgary.ca>
2022-01-19 08:19:50 -0800

    add notes

--- my_notes/day_1.R
+++ my_notes/day_1.R
@@ -1,4 +1,4 @@
-#class notes
+#class exercises
 
 #load packages
 library(tidyverse)
@@ -11,7 +11,24 @@ dice <- read.csv("data/loaded_dice.csv") %>%
 y <- pull(dice, is_six) #grab the vector of data from the dice_df
 
 #do a model - create a new stan file via File > New File > Stan File
-dice_1_model <- stan('my_notes/dice_1.stan', data = list(y = y, N = length(y)))
-  #R knows 
-
+dice_1_model <- stan('my_notes/dice_1.stan', data = list(y=y, N=length(y)))
 dice_1_model
+
+dice_2_data <- list(y=y, N=length(y), a=2, b=3) #write the data as a list object rather
+                                                  #than in function as above
+dice_2_model <- stan('my_notes/dice_2.stan', data=dice_2_data)
+dice_2_model
+
+#next do a loop on theta model to speed up C++
+dice_3_model <- stan('my_notes/dice_3.stan', data=dice_2_data)
+dice_3_model
+
+#then do a binomial instead of bernoulli 
+dice_4_data <- list(m=sum(y), N=length(y), a=2, b=3) #write the data as a list object rather
+dice_4_model <- stan('my_notes/dice_4.stan', data=dice_4_data)
+dice_4_model
+
+#then logistic - write it out as log odds 
+dice_5_data <- list(y=y, N=length(y), sigma = 1) #write the data as a list object rather
+dice_5_model <- stan('my_notes/dice_5.stan', data=dice_5_data) #WHY ERROR!?
+dice_5_model #getting posterior over mu, but want it or theta
